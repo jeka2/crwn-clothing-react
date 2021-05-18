381fedec1ae4ea2fa7009f93c2339fda91043a45
Yev <anigeka@gmail.com>
2021-05-18 14:05:00 -0400

    add a shop page with categories

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import './App.css';
 import { Link, Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-    <Link to="/">Home</Link>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ HomePage } />
-        <Route path='/hats' component={ HatsPage } />
+        <Route path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
