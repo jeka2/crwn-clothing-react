81c317703902be4081bb8087c43633ba512ea0fd
Yev <anigeka@gmail.com>
2021-05-18 12:10:29 -0400

    add basic menu item routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Link, Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+    <Link to="/">Home</Link>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route path='/hats' component={ HatsPage } />
+      </Switch>
     </div>
   );
 }
